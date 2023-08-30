e0ebf61609d4b95ef11aa86f5a0549e4951946bb
Stepan <kmetuk60187@gmail.com>
2023-08-30 23:35:02 +0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+export default {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'eslint:recommended',
+
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    indent: ['error', 'tab'],
+    'linebreak-style': ['error', 'windows'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'no-unused-vars': ['warn'],
+  },
+};
---
+++ rsc/main.js
@@ -0,0 +1,43 @@
+'use strict';
+//? початок роботи 6-й урок:
+// npm init
+// npm init @eslint/config
+//? =====================================================
+// snake_case
+// UPPER_SNAKE_CASE
+// Kebab-case
+// PasalCase // first letter BIG;
+//?======================================================
+// number 1,2,3
+// string 'str', 'Stepan'
+// boolean true/felse
+// null   // pystota
+// undefined
+// Simbol
+// bigint
+// ObJeKTs:
+// object [], function, object Data, RegEx, Error
+//=======================================================
+// interpoliacia
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/5`);
+//?=======================================================
+
+//? lesson15 ================
+// плюч перед виразом називається унарний плюс +(str) переториться в число
+// console.log(4 + +"4")
+
+// let incr = 10,
+//   decr = 10;
+
+// постфіксная форма
+// incr++; // збільшення числа не 1 incriment
+// decr--; // зьеншення числа не 1  decriment
+
+// префіксная форма
+// ++incr;
+// --decr;
+// console.log(incr)
+// console.log(decr)
+//? ============================================================================
+//? lesson16 GitHub ================
