f9904f8df04424ca535a991268ae970da564fdb8
Stepan <kmetuk60187@gmail.com>
2023-09-01 01:04:59 +0300

    add first program

--- main.js
+++ main.js
@@ -67,12 +67,22 @@
 
 'use strict';
 
-let numberOfFilms = 'Скільки фільмів ви подивилися??';
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-let personalMovieDB = {
-  count: 'Я посмотрел 4 фильма',
+const personalMovieDB = {
+  count: numberOfFilms,
   movies: {},
   actors: {},
   genres: [],
   privat: false,
 };
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+  b = prompt('На сколько оцените его?', ''),
+  c = prompt('Один из последних просмотренных фильмов?', ''),
+  d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
