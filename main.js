6abe8fd4bbaccc602f2cdc54afea995089f64a01
Stepan <kmetuk60187@gmail.com>
2023-09-05 00:50:30 +0300

    repeat atcions

--- main.js
+++ main.js
@@ -119,18 +119,39 @@
 //   i++;
 // }
 
-// Место для первой задачи
-function firstTask() {
-  // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
-  const arr = [3, 5, 8, 16, 20, 23, 50];
-  const result = [];
+// Место для третьей задачи
+// function thirdTask() {
+//   const data = [5, 10, 'Shopping', 20, 'Homework'];
+//   const result = [];
 
-  // Пишем решение вот тут
-  for (let i = 0; i < arr.length; i++) {
-    result[i] = arr[i];
-  }
-  console.log(result);
-  // Не трогаем
-  return result;
-}
-firstTask();
+//   for (let i = 1; i <= data.length; i++) {
+//     result[i - 1] = data[data.length - i]
+//   }
+
+//   console.log(result)
+//   return result;
+// }
+// thirdTask();
+
+// const lines = 4;
+// let result = '';
+
+// for (let i = 0; i <= lines; i++) {
+//   for (let j = 0; j < lines - i; j++) {
+//     result += ' ';
+//   }
+//   for (let j = 0; j < 2 * i + 1; j++) {
+//     result += '*';
+//   }
+//   result += '\n';
+// }
+
+// console.log(result);
+// const lines = 5;
+// let result = '';
+// for (let i = 0; i < lines; i++) {
+//   for (let j = 0; j < lines - i; j++) {
+//     result += ' ';
+//   }
+//   console.log(result)
+// }
